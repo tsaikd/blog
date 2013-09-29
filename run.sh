32ed26c5a62a3e6c3af4360ce4e02c0bf29470c9
tsaikd <tsaikd@gmail.com>
2013-09-29 22:53:40 +0800

    add more soft link in run.sh for fixing deploy on folder

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ pushd "${PD}"
 rm -rf .tmp_partials/ compiled/
 gor compile
 ln -s ../assets/ compiled/blog/
+ln -s ./ compiled/blog/blog
+ln -s ../rss.xml compiled/blog/
+ln -s ../sitemap.xml compiled/blog/
 gor http
 
 popd
