93001f0cf15f39dd2c1015035dab36b158defe16
tsaikd <tsaikd@gmail.com>
2014-01-26 00:56:00 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,6 @@ pushd "${PD}"
 
 rm -rf .tmp_partials/ compiled/
 gor compile
-ln -s ../assets/ compiled/blog/
-ln -s ./ compiled/blog/blog
-ln -s ../rss.xml compiled/blog/
-ln -s ../sitemap.xml compiled/blog/
-gor http
 
 popd
 
