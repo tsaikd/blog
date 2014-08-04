b789e1c0846bdcc7c3851ff880d453adf592975c
tsaikd <tsaikd@gmail.com>
2014-08-04 23:53:33 +0800

    update run.sh to auto compile and upload to github

--- run.sh
+++ run.sh
@@ -3,11 +3,19 @@
 PN="${BASH_SOURCE[0]##*/}"
 PD="${BASH_SOURCE[0]%/*}"
 
-pushd "${PD}"
+pushd "${PD}" || exit $?
 
-rm -rf .tmp_partials/ compiled/blog/*
-gor compile
-chown 1000:1000 -R .tmp_partials/ compiled/blog/
+	rm -rf .tmp_partials/ compiled/blog/*
+	gor compile || exit $?
+	chown 1000:1000 -R .tmp_partials/ compiled/blog/ || exit $?
 
-popd
+	pushd compiled/blog || exit $?
+
+		git add -A || exit $?
+		git commit -m "$(date): Auto update" --author "tsaikd <tsaikd@gmail.com>" || exit $?
+		git push origin gh-pages:gh-pages || exit $?
+
+	popd || exit $?
+
+popd || exit $?
 
